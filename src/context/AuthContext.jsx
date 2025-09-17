171f97d41e55bc5ec14e54af19c1fa658012da0f
Tarun Singh <tarun.singh.7801@gmail.com>
2025-09-17 17:04:29 +0530

    final

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,11 +1,8 @@
 import React, { createContext, useContext, useState } from "react";
-
 const AuthContext = createContext();
-
 export const useAuth = () => {
   return useContext(AuthContext);
 };
-
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
   const [role, setRole] = useState(null);
@@ -22,7 +19,6 @@ export const AuthProvider = ({ children }) => {
     setRole(null);
     setIsAuthenticated(false);
   };
-
   return (
     <AuthContext.Provider
       value={{ token, role, isAuthenticated, login, logout }}
